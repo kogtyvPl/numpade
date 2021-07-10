033fa9424ee449637f1e75df6e5ed9a6c6711bac
kogtyvPl <85754331+kogtyvPl@users.noreply.github.com>
2021-07-10 18:15:08 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,46 +4,68 @@ local GUI = require("GUI")
 local system = require("System")
 local fs = require("Filesystem")
 
+local scrol = {}
+local lines = {""}
 local textzg = ""
 local codeView = ""
+local cvhei = 40
+local cvwid = 48
 ---------------------------------------------------------------------------------
 
 -- Add a new window to MineOS workspace
 local workspace, window, menu = system.addWindow(GUI.filledWindow(1, 1, 60, 45, 0xE1E1E1))
+local workspace2, window2 = system.addWindow(GUI.filledWindow(65, 1, 50, 45, 0xCC9280))
 
 -- Get localization table dependent of current system language
 --local localization = system.getCurrentScriptLocalization() (залупа закоментированая)
 
 
-
 -- Add single cell layout to window
 local layout = window:addChild(GUI.layout(1, 1, window.width, window.height, 1, 1))
+local layout2 = window2:addChild(GUI.layout(1, 1, window2.width, window2.height, 1, 1))
 
 -- Add nice gray text object to layout
 layout:addChild(GUI.text(1, 1, 0x4B4B4B, "Привет, напиши свой текст, " .. system.getUser()))
+
+local function addButton2(text2)
+return layout2:addChild(GUI.roundedButton(1, 1, 36, 3, 0xD2D2D2, 0x696969, 0x4B4B4B, 0xF0F0F0, text2))
+end
 
 local function addButton(text)
 return layout:addChild(GUI.roundedButton(1, 1, 36, 3, 0xD2D2D2, 0x696969, 0x4B4B4B, 0xF0F0F0, text))
 end
 
-
 local namefile = layout:addChild(GUI.input(15, 21, 30, 3, 0xEEEEEE, 0x555555, 0x999999, 0xFFFFFF, 0x2D2D2D, "MyText.txt", "Имя файла"))
-  
-local codeView = layout:addChild(GUI.codeView(2, 2, 0, 0, 1, 1, 1, {}, {}, GUI.LUA_SYNTAX_PATTERNS, GUI.LUA_SYNTAX_COLOR_SCHEME, true, {}))
+
+local codeView = layout2:addChild(GUI.codeView(2, 2, 0, 0, 1, 1, 1, scrol, {}, GUI.LUA_SYNTAX_PATTERNS, GUI.LUA_SYNTAX_COLOR_SCHEME, true, lines))
 
 local lable = layout:addChild(GUI.input(15, 15, 30, 3, 0xEEEEEE, 0x555555, 0x999999, 0xFFFFFF, 0x2D2D2D, textzg, "Напишите сюда текст"))
 addButton("Записать изменения.txt").onTouch = function()
   if #lable.text > 0 then
     --GUI.alert(lable.text, " - этот текст был сохранён в файл")
     fs.append("/notepad/" .. namefile.text, "\n" .. lable.text)
+    codeView:remove()
+    local codeView = layout2:addChild(GUI.codeView(2, 2, cvwid, cvhei, 1, 1, 1, scrol, {}, GUI.LUA_SYNTAX_PATTERNS, GUI.LUA_SYNTAX_COLOR_SCHEME, true, {}))
+    local counter = 1
+    for line in require("filesystem").lines("/notepad/" .. namefile.text) do
+  -- Replace tab symbols to 2 whitespaces and Windows line endings to UNIX line endings
+      line = line:gsub("\t", "  "):gsub("\r\n", "\n")
+      codeView.maximumLineLength = math.max(codeView.maximumLineLength, unicode.len(line))
+      table.insert(codeView.lines, line)
+
+      counter = counter + 1
+      if counter > codeView.height then
+        break
+      end
+    end
   else
     GUI.alert("Пустая строка.")
   end
 end
 addButton("Загрузить файл.txt").onTouch = function()
-  codeView:remove()
-  local codeView = layout:addChild(GUI.codeView(2, 2, 45, 24, 1, 1, 1, {}, {}, GUI.LUA_SYNTAX_PATTERNS, GUI.LUA_SYNTAX_COLOR_SCHEME, true, {}))
   if #namefile.text > 0 then
+    codeView:remove()
+    local codeView = layout2:addChild(GUI.codeView(2, 2, cvwid, cvhei, 1, 1, 1, scrol, {}, GUI.LUA_SYNTAX_PATTERNS, GUI.LUA_SYNTAX_COLOR_SCHEME, true, {}))
     local counter = 1
     for line in require("filesystem").lines("/notepad/" .. namefile.text) do
   -- Replace tab symbols to 2 whitespaces and Windows line endings to UNIX line endings
@@ -60,8 +82,33 @@ addButton("Загрузить файл.txt").onTouch = function()
     GUI.alert("Ошибка! Вы не можете загрузить не существующий файл!!!")
   end
 end
+addButton("Удалить файл").onTouch = function()
+  fs.remove("/notepad/" .. namefile.text)
+  GUI.alert("Файл /notepad/" .. namefile.text .. " удалён успешно.")
+end
+layout:addChild(GUI.text(1, 1, 0x4B4B4B, "Размер холста"))
+addButton("24x12").onTouch = function()
+  cvhei = 12
+  cvwid = 24 
+end
+addButton("45x24").onTouch = function()
+  cvhei = 24
+  cvwid = 45
+end
+addButton("48x40").onTouch = function()
+  cvhei = 40
+  cvwid = 48
+end
+addButton("100x40 - не удобен!").onTouch = function()
+  cvhei = 40
+  cvwid = 100
+end
+layout2:addChild(GUI.text(1, 1, 0x4B4B4B, "Отображение файла"))
 
-layout:addChild(GUI.text(1, 1, 0x4B4B4B, "Отображение файла"))
+--local verticalScrollBar = layout:addChild(GUI.scrollBar(2, 3, 1, 15, 0x444444, 0x888888, 1, 100, 1, 10, 1, true))
+--verticalScrollBar.onTouch = function()
+--  GUI.alert("Вертикальную полосу тронули.")
+--end
 
 --local codeView = layout:addChild(GUI.codeView(2, 2, 45, 24, 1, 1, 1, {}, {}, GUI.LUA_SYNTAX_PATTERNS, GUI.LUA_SYNTAX_COLOR_SCHEME, true, {}))
 
@@ -96,6 +143,11 @@ end
 -- Create callback function with resizing rules when window changes its' size
 window.onResize = function(newWidth, newHeight)
   window.backgroundPanel.width, window.backgroundPanel.height = newWidth, newHeight
+  layout.width, layout.height = newWidth, newHeight
+end
+
+window2.onResize = function(newWidth, newHeight)
+  window2.backgroundPanel.width, window2.backgroundPanel.height = newWidth, newHeight
   layout.width, layout.height = newWidth, newHeight
 end
 
